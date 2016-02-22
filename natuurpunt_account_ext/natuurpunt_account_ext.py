d54b85e3054ec2728a1ed02e40c4b3df3d50ccc4
Joeri Belis <joeri.belis@natuurpunt.be>
2016-02-22 14:18:59 +0100

    [FIX] #2297 - force _amount_all via _store_set_function

--- natuurpunt_account_ext/natuurpunt_account_ext.py
+++ natuurpunt_account_ext/natuurpunt_account_ext.py
@@ -28,26 +28,44 @@ class account_invoice(osv.osv):
 
     _inherit = 'account.invoice'
 
+    def _store_set_values(self, cr, uid, ids, fields, context=None):
+        """force multi function_field 'amount_total_signed' to use the _amount_all function
+        of the inherited account_invoice class
+        """        
+        return super(account_invoice, self)._store_set_values(
+                cr, uid, ids, 
+                sorted(fields, key = lambda x: 0 if x=='amount_total_signed' else 1), context)
+ 
     def _amount_all(self, cr, uid, ids, name, args, context=None):
         res = {}
         for invoice in self.browse(cr, uid, ids, context=context):
             res[invoice.id] = {
                 'amount_untaxed': 0.0,
                 'amount_tax': 0.0,
-                'amount_total': 0.0
+                'amount_total': 0.0,
                 'amount_total_signed': 0.0
             }
-            if invoice.type in ['in_refund','out_refund']:
-                res[invoice.id]['amount_total_signed'] = -invoice.amount_total
-            else:
-                res[invoice.id]['amount_total_signed'] = invoice.amount_total
-
             for line in invoice.invoice_line:
                 res[invoice.id]['amount_untaxed'] += line.price_subtotal
             for line in invoice.tax_line:
                 res[invoice.id]['amount_tax'] += line.amount
             res[invoice.id]['amount_total'] = res[invoice.id]['amount_tax'] + res[invoice.id]['amount_untaxed']
+
+            sign = -1 if invoice.type in ['in_refund','out_refund'] else 1
+            res[invoice.id]['amount_total_signed'] = res[invoice.id]['amount_total'] * sign
         return res
+
+    def _get_invoice_line(self, cr, uid, ids, context=None):
+        result = {}
+        for line in self.pool.get('account.invoice.line').browse(cr, uid, ids, context=context):
+            result[line.invoice_id.id] = True
+        return result.keys()
+
+    def _get_invoice_tax(self, cr, uid, ids, context=None):
+        result = {}
+        for tax in self.pool.get('account.invoice.tax').browse(cr, uid, ids, context=context):
+            result[tax.invoice_id.id] = True
+        return result.keys()
 
     _columns = {
         'amount_total_signed': fields.function(_amount_all, digits_compute=dp.get_precision('Account'), string='Totaal (+/-)',
@@ -59,5 +77,7 @@ class account_invoice(osv.osv):
             multi='all'),
 
     }
+
+account_invoice()
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
