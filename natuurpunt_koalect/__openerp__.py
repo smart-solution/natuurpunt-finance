c04ae008848d37a2812dcf71965611ed5c2ae981
joeribelis <joeri.belis@natuurpunt.be>
2017-08-04 15:23:53 +0200

    [IMP] #3217 - koalect UI

---
+++ natuurpunt_koalect/__openerp__.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#
+##############################################################################
+{
+    "name" : "natuurpunt_koalect",
+    "version" : "1.0",
+    "author" : "Joeri Belis",
+    "category" : "Generic Modules/Base",
+    "description": """
+""",
+    "depends" : ["base",],
+    "update_xml" : [
+        'natuurpunt_koalect_view.xml',
+        ],
+    "active": False,
+    "installable": True
+}
