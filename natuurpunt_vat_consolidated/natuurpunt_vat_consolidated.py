871dad195c873bfa9540d1b5b1d8900cb2c37f85
joeribelis <joeri.belis@natuurpunt.be>
2017-12-15 09:04:12 +0100

    [FIX] #5310 first cumul than report. vak 71 resp. 72 is unique on report

--- natuurpunt_vat_consolidated/natuurpunt_vat_consolidated.py
+++ natuurpunt_vat_consolidated/natuurpunt_vat_consolidated.py
@@ -130,13 +130,7 @@ class natuurpunt_vat_consolidated(osv.osv_memory):
                 ctx = context.copy()
                 ctx['period_id'] = account_period.id
                 for tax_code in obj_tax_code.read(cr, uid, tax_code_ids, ['code','sum_period'], context=ctx):
-                    if tax_code['code'] == 'VI':
-                        if tax_code['sum_period'] >= 0:
-                            tax_counter['71'] += abs(tax_code['sum_period'])
-                        else:
-                            tax_counter['72'] += abs(tax_code['sum_period'])
-                    else:
-                        tax_counter[tax_code['code']] += abs(tax_code['sum_period'])
+                    tax_counter[tax_code['code']] += tax_code['sum_period']
         tax_info = [{'code': code, 'sum_period': sum_period} for code, sum_period in tax_counter.items()]
 
         data_of_file = """<?xml version="1.0"?>
@@ -179,8 +173,14 @@ class natuurpunt_vat_consolidated(osv.osv_memory):
             if item['code'] == '91' and ending_month != 12:
                 #the tax code 91 can only be send for the declaration of December
                 continue
-            if item['code'] and item['sum_period'] and item['code'] in list_of_tags:
-                cases_list.append(item)
+            if item['code'] and item['sum_period']:
+                if item['code'] == 'VI':
+                    if item['sum_period'] >= 0:
+                        item['code'] = '71'
+                    else:
+                        item['code'] = '72'
+                if item['code'] in list_of_tags:
+                    cases_list.append(item)
         cases_list.sort()
         for item in cases_list:
             grid_amount_data = {
