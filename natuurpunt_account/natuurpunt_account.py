645d781c21e4f805fab5bb3c735e5e4b493c13fb
mattiasvo <mattias.vanopstal@natuurpunt.be>
2018-03-13 12:06:08 +0100

    [#1605] replace default_code by id  (product)

--- natuurpunt_account/natuurpunt_account.py
+++ natuurpunt_account/natuurpunt_account.py
@@ -151,7 +151,6 @@ class account_invoice(osv.osv):
         """Clear the supplier reference at copy"""
         default['supplier_invoice_number'] = False
         default['reference'] = False
-        default['origin'] = False
         return super(account_invoice, self).copy(cr, uid, id, default=default, context=context)
 
     def onchange_date_invoice(self, cr, uid, ids, invoice_date, payment_term, context=None):
@@ -513,18 +512,18 @@ class payment_line(osv.osv):
     }
 
 class product_product(osv.osv):
-
-
+ 
+ 
     _inherit = "product.product"
-
-    def create(self, cr, uid, vals, context=None):
-        """Add the np sequence referece"""
-        seq_id = self.pool.get('ir.sequence').search(cr, uid, [('code','=','res.product.np.ref')])
-        print seq_id
-        vals['default_code'] = self.pool.get('ir.sequence').next_by_id(cr, uid, seq_id, context)
-        print vals
-        vals['company_id'] = False
-        return super(product_product, self).create(cr, uid, vals, context=context)
+ 
+#     def create(self, cr, uid, vals, context=None):
+#         """Add the np sequence referece"""
+#         seq_id = self.pool.get('ir.sequence').search(cr, uid, [('code','=','res.product.np.ref')])
+#         print seq_id
+#         vals['default_code'] = self.pool.get('ir.sequence').next_by_id(cr, uid, seq_id, context)
+#         print vals
+#         vals['company_id'] = False
+#         return super(product_product, self).create(cr, uid, vals, context=context)
 
 
 class account_journal(osv.osv):
